cfbb62e631321af5f2ff984a55d728c33c3ece48
Stéphanie lemaitre <stephanie.lemaitre56@gmail.com>
2020-06-15 18:33:44 +0200

    mise a jour

--- app/views/frontEnd/pages/australie.php
+++ app/views/frontEnd/pages/australie.php
@@ -6,7 +6,7 @@
 
 <!-- Appel des templates des articles, footer -->
 <?php require 'app/views/frontEnd/articles/articlesForm.php'; ?>
-<?php include 'app/views/frontEnd/templates/footer.php'; ?>
+<?php require 'app/views/frontEnd/templates/footer.php'; ?>
 
 <!-- Script du menu Activ -->
 <script>menuAct(1); </script>
